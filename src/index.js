747b6f462eb53d845724e848892f8b63dbff7ccd
Alina <astapchik.1993@mail.ru>
2022-08-01 14:41:22 +0300

    feat: task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+   n = Math.abs(n)
+   let array1 = Array.from(n.toString(), Number)
+   console.log(array1);
+   let array2 = [];
+   array2.length = array1.length
+   console.log(array2.length)
+   for (let i = 0; i<array2.length; i++) {
+      array2[i]=array1[array1.length-1-i]
+   }
+   console.log(array2);
+   let newNumber = +array2.join('')
+   return newNumber
 }
